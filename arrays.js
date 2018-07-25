26fb7597dbccc4798b32b5a58dedd88ea6247775
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-07-25 18:29:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,7 @@ var chocolateBars = [
     return array;
   }
   
-  function removeElementFromBeginningOfArray(array, element) {
+  function removeElementFromBeginningOfArray(array) {
     array.slice(1);
     return array;
   }
